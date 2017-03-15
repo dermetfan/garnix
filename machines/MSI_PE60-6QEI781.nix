0b2f0886c56058d2fcfda4e5a18042b2e2ce1fc5
Robin Stumm <serverkorken@gmail.com>
2017-03-15 22:40:28 +0100

    add common config for all MSI PE60-6QEI781 laptops

---
+++ machines/MSI_PE60-6QEI781.nix
@@ -0,0 +1,74 @@
+{ config, lib, pkgs, ... }:
+
+let
+  bumblebee = pkgs.writeScript "bumblebee.sh" ''
+    ${pkgs.bumblebee}/bin/bumblebeed -Dg ${config.users.groups.wheel.name}
+  '';
+in {
+  imports = [
+    <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
+  ];
+
+  boot = {
+    initrd.availableKernelModules = [
+      "xhci_pci"
+      "ahci"
+      "usb_storage"
+      "usbhid"
+      "sd_mod"
+      "sr_mod"
+      "rtsx_usb_sdmmc"
+    ];
+  
+    kernelModules = [
+      "kvm-intel"
+      "bbswitch"
+    ];
+
+    blacklistedKernelModules = [
+      "nouveau" # causes CPU stalls with intel GPU driver
+    ];
+
+    extraModulePackages = with pkgs.linuxPackages; [
+      bbswitch
+    ];
+
+    loader = {
+      systemd-boot.enable = true;
+      grub = {
+        efiSupport = true;
+        zfsSupport = true;
+        configurationLimit = 25;
+      };
+    };
+  };
+
+  nix.maxJobs = lib.mkDefault 8;
+
+  hardware = {
+    enableAllFirmware = true;
+    # nvidiaOptimus.disable = true;
+    bumblebee = {
+      # enable = true;
+      driver = "nvidia";
+    };
+    opengl.driSupport32Bit = true;
+    bluetooth.enable = true;
+    pulseaudio = {
+      package = pkgs.pulseaudioFull;
+      support32Bit = true;
+    };
+  };
+
+  security.sudo.extraConfig = ''
+    %${config.users.groups.wheel.name} ALL=(ALL:ALL) NOPASSWD:${bumblebee}
+  '';
+
+  services.xserver = {
+    displayManager.sessionCommands = ''
+      sudo ${bumblebee}
+    '';
+
+    videoDrivers = [ "intel" ];
+  };
+}
