537068debf52fb7341dc52cc110eb04cb5037632
Robin Stumm <serverkorken@gmail.com>
2017-07-18 15:56:18 +0200

    gtk: fix SVG rendering

--- .config/nixpkgs/home/programs/gtk.nix
+++ .config/nixpkgs/home/programs/gtk.nix
@@ -1,7 +1,20 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-  home.file.".config/gtk-3.0/bookmarks".text = ''
-    file:///data/dermetfan
-  '';
+  home = {
+    file.".config/gtk-3.0/bookmarks".text = ''
+      file:///data/dermetfan
+    '';
+
+    sessionVariables.GDK_PIXBUF_MODULE_FILE =
+      let
+        v2 = "${pkgs.librsvg.out}/lib/gdk-pixbuf-2.0";
+        v2v = builtins.elemAt (
+          builtins.attrNames (
+            builtins.readDir v2
+          )
+        ) 0;
+      in "${v2}/${v2v}/loaders.cache";
+  };
 }
+
