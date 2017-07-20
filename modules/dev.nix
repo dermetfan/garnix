fff5c67db7a35efb49df317f389b6b936d8bb919
Robin Stumm <serverkorken@gmail.com>
2017-07-20 17:13:50 +0200

    nixos-containers: prevent NetworkManager from managing NixOS containers

--- modules/dev.nix
+++ modules/dev.nix
@@ -1,14 +1,18 @@
 { config, ... }:
 
 {
-  networking.nat = {
-    enable = true;
-    internalInterfaces = [
-      "ve-+" # nixos-containers
-    ];
-    externalInterface = config.passthru.hardware.interfaces.lan
-      or config.passthru.hardware.interfaces.wlan
-      or null;
+  networking = {
+    nat = {
+      enable = true;
+      internalInterfaces = [
+        "ve-+" # nixos-containers
+      ];
+      externalInterface = config.passthru.hardware.interfaces.lan
+        or config.passthru.hardware.interfaces.wlan
+        or null;
+    };
+
+    networkmanager.unmanaged = [ "interface-name:ve-*" ];
   };
 
   programs.adb.enable = true;
