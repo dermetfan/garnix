a2200d97ca49277ea3fd541897b7df70fa27c7a3
Robin Stumm <serverkorken@gmail.com>
2017-08-14 21:25:46 +0200

    don't install all those fonts

--- profiles/notebook.nix
+++ profiles/notebook.nix
@@ -30,21 +30,6 @@
   fonts = {
     enableFontDir = true;
     enableGhostscriptFonts = true;
-    fonts = with pkgs; [
-      corefonts
-      dejavu_fonts
-      hack-font
-      ucs-fonts
-      libertine
-      nerdfonts
-      proggyfonts
-      terminus_font
-      anonymousPro
-      font-awesome-ttf
-      fira
-      fira-code
-      fira-mono
-    ];
   };
 
   hardware.pulseaudio.enable = false; # does not work with sound.mediaKeys.enable because amixer cannot connect to PulseAudio user daemon as another user (root) => share PulseAudio cookie?
