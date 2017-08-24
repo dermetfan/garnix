fb51b21219ac675aecd8e6ccfd43d7d3b18411f2
Robin Stumm <serverkorken@gmail.com>
2017-08-24 20:37:46 +0200

    MSI PE60: bumblebee hack

--- machines/MSI_PE60-6QEI781.nix
+++ machines/MSI_PE60-6QEI781.nix
@@ -1,9 +1,12 @@
 { config, lib, pkgs, ... }:
 
 let
-  bumblebee = pkgs.writeScript "bumblebee.sh" ''
-    ${pkgs.bumblebee}/bin/bumblebeed -Dg ${config.users.groups.wheel.name}
-  '';
+  bumblebeeHack = {
+    enable = false;
+    bumblebeed = pkgs.writeScript "bumblebeed.sh" ''
+      ${pkgs.bumblebee}/bin/bumblebeed -Dg ${config.users.groups.wheel.name}
+    '';
+  };
 in rec {
   imports = [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix> ];
 
@@ -20,18 +23,15 @@ in rec {
       "rtsx_usb_sdmmc"
     ];
 
-    kernelModules = [
-      "kvm-intel"
-      "bbswitch"
-    ];
+    kernelModules = [ "kvm-intel" ] ++
+      lib.optional bumblebeeHack.enable "bbswitch";
 
     blacklistedKernelModules = [
       "nouveau" # causes CPU stalls with intel GPU driver
     ];
 
-    extraModulePackages = with pkgs.linuxPackages; [
-      bbswitch
-    ];
+    extraModulePackages = with pkgs.linuxPackages;
+      lib.optional bumblebeeHack.enable bbswitch;
 
     tmpOnTmpfs = true;
   };
@@ -43,11 +43,8 @@ in rec {
 
   hardware = {
     enableAllFirmware = true;
-    # nvidiaOptimus.disable = true;
-    bumblebee = {
-      # enable = true;
-      driver = "nvidia";
-    };
+#    nvidiaOptimus.disable = true;
+#    bumblebee.enable = true;
     opengl.driSupport32Bit = true;
     bluetooth.enable = true;
     pulseaudio = {
@@ -56,13 +53,13 @@ in rec {
     };
   };
 
-  security.sudo.extraConfig = ''
-    %${config.users.groups.wheel.name} ALL=(ALL:ALL) NOPASSWD:${bumblebee}
+  security.sudo.extraConfig = lib.optionalString bumblebeeHack.enable ''
+    %${config.users.groups.wheel.name} ALL=(ALL:ALL) NOPASSWD:${bumblebeeHack.bumblebeed}
   '';
 
   services.xserver = {
-    displayManager.sessionCommands = ''
-      sudo ${bumblebee}
+    displayManager.sessionCommands = lib.optionalString bumblebeeHack.enable ''
+      sudo ${bumblebeeHack.bumblebeed}
     '';
 
     videoDrivers = [ "intel" ];
