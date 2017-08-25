2c101505bcd0222309c06b13c1cfd00af809aa48
Robin Stumm <serverkorken@gmail.com>
2017-08-25 03:08:53 +0200

    antigen: install

--- .config/nixpkgs/home/programs/antigen.nix
+++ .config/nixpkgs/home/programs/antigen.nix
@@ -1,36 +1,41 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 {
-  home.file.".antigenrc".text = ''
-    antigen use oh-my-zsh
+  home.file = {
+    ".antigenrc".text = ''
+      antigen use oh-my-zsh
 
-    antigen theme nicoulaj
+      antigen theme nicoulaj
 
-    antigen bundles <<EOS
-      zsh-users/zsh-syntax-highlighting
-      colored-man-pages
-      dircycle
-      extract
-      history
-      sudo
-      mosh
-      tmux
-      urltools
-      gradle
-      mercurial
-      git
-      nyan
-    EOS
-    antigen bundle web-search; alias open_command=xdg-open
+      antigen bundles <<EOF
+        zsh-users/zsh-syntax-highlighting
+        colored-man-pages
+        dircycle
+        extract
+        history
+        sudo
+        mosh
+        tmux
+        urltools
+        gradle
+        mercurial
+        git
+        nyan
+      EOF
+      antigen bundle web-search; alias open_command=xdg-open
 
-    antigen apply
-  '';
+      antigen apply
+    '';
+
+    ".antigen/antigen.zsh".source = pkgs.fetchurl {
+      url = https://cdn.rawgit.com/zsh-users/antigen/v2.2.1/bin/antigen.zsh;
+      sha512 = "f1551060bf9756c3b7881a22584986a0528d9b3c5acfedb9ea912d04901419db537f8c8e15fdd6539aa26a91175a59b7534bd83a03268db9f19cadc0331a523e";
+    };
+  };
 
   programs.zsh.initExtra = ''
-    if [[ -f ~/.antigen/antigen.zsh ]]; then
-        . ~/.antigen/antigen.zsh
-        antigen init ~/.antigenrc
-    fi
+    . ~/.antigen/antigen.zsh
+    antigen init ~/.antigenrc
 
     ${# apply aliases again, antigen may have changed them
       lib.concatStringsSep "\n" (
