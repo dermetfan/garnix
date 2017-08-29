f1553f2d8e63594d6ad58c5d960a54928a0fe3f3
Robin Stumm <serverkorken@gmail.com>
2017-08-30 00:18:26 +0200

    lilyterm: whitelist nano and micro

--- .config/nixpkgs/home/programs/lilyterm.nix
+++ .config/nixpkgs/home/programs/lilyterm.nix
@@ -65,7 +65,7 @@ in {
 
         # Don't need to confirm for executing a program if it's in the whitelist,
         # separate with <space>.
-        execute_command_whitelist = ssh mosh tmux htop ranger alsamixer
+        execute_command_whitelist = ssh mosh tmux htop ranger nano micro alsamixer
 
         # Launching executed command in a new tab instead of opening a new window.
         execute_command_in_new_tab = 0
