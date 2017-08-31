661762b3ab381727049cb7bc18df9a6b5276030e
Robin Stumm <serverkorken@gmail.com>
2017-08-31 04:26:49 +0200

    home: include module-list.nix

---
+++ .config/nixpkgs/home/module-list.nix
@@ -0,0 +1,36 @@
+[
+  profiles/desktop.nix
+  profiles/effects.nix
+  profiles/media.nix
+  profiles/dev.nix
+  profiles/admin.nix
+  profiles/office.nix
+  profiles/game.nix
+
+  misc/gtk.nix
+  misc/xdg.nix
+
+  programs/antigen.nix
+  programs/cargo.nix
+  programs/compton.nix
+  programs/dunst.nix
+  programs/geany.nix
+  programs/htop.nix
+  programs/i3.nix
+  programs/i3status.nix
+  programs/lilyterm.nix
+  programs/mercurial.nix
+  programs/minecraft.nix
+  programs/nano.nix
+  programs/nitrogen.nix
+  programs/parcellite.nix
+  programs/ranger.nix
+  programs/rofi.nix
+  programs/volumeicon.nix
+  programs/xfe.nix
+  programs/xpdf.nix
+  programs/xscreensaver.nix
+  programs/zr.nix
+] ++ (if builtins.pathExists profiles/local.nix then [
+  profiles/local.nix
+] else [])
