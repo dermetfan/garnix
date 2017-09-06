3a239c49292e8f6098c20cabefb4b071cbf83915
Robin Stumm <serverkorken@gmail.com>
2017-09-06 19:18:45 +0200

    MSI PE60-6QE: better bumblebee hack
    Use the existing NixOS service

--- modules/machines/MSI_PE60-6QE.nix
+++ modules/machines/MSI_PE60-6QE.nix
@@ -3,10 +3,6 @@
 
 let
   cfg = config.config.machine."MSI PE60-6QE";
-
-  bumblebeeHack = pkgs.writeScript "bumblebeed.sh" ''
-    ${pkgs.bumblebee}/bin/bumblebeed -Dg ${config.users.groups.wheel.name}
-  '';
 in {
   options.config.machine."MSI PE60-6QE" = {
     enable = lib.mkEnableOption "MSI PE60-6QE";
@@ -24,60 +20,86 @@ in {
     enableBumblebeeHack = lib.mkEnableOption "the Bumblebee hack to turn off the dedicated GPU";
   };
 
-  config = lib.mkIf cfg.enable {
-    nixpkgs.config.allowUnfree = true;
+  config = lib.mkIf cfg.enable (lib.mkMerge [
+    {
+      nixpkgs.config.allowUnfree = true;
 
-    boot = {
-      initrd.availableKernelModules = [
-        "xhci_pci"
-        "ahci"
-        "usb_storage"
-        "usbhid"
-        "sd_mod"
-        "sr_mod"
-        "rtsx_usb_sdmmc"
-      ];
+      boot = {
+        initrd.availableKernelModules = [
+          "xhci_pci"
+          "ahci"
+          "usb_storage"
+          "usbhid"
+          "sd_mod"
+          "sr_mod"
+          "rtsx_usb_sdmmc"
+        ];
 
-      kernelModules = [ "kvm-intel" ] ++
-        lib.optional cfg.enableBumblebeeHack "bbswitch";
+        kernelModules = [ "kvm-intel" ];
 
-      blacklistedKernelModules = [
-        "nouveau" # causes CPU stalls with intel GPU driver
-      ];
+        blacklistedKernelModules = [
+          "nouveau" # causes CPU stalls with intel GPU driver
+        ];
 
-      extraModulePackages = with pkgs.linuxPackages;
-        lib.optional cfg.enableBumblebeeHack bbswitch;
-
-      tmpOnTmpfs = true;
-    };
-
-    nix = {
-      maxJobs = if cfg.hyperThreading then 8 else 4;
-      buildCores = if cfg.hyperThreading then 8 else 4;
-    };
-
-    hardware = {
-      enableAllFirmware = true;
-#      nvidiaOptimus.disable = true;
-#      bumblebee.enable = true;
-      opengl.driSupport32Bit = true;
-      bluetooth.enable = true;
-      pulseaudio = {
-        package = pkgs.pulseaudioFull;
-        support32Bit = true;
+        tmpOnTmpfs = true;
       };
-    };
 
-    security.sudo.extraConfig = lib.optionalString cfg.enableBumblebeeHack ''
-      %${config.users.groups.wheel.name} ALL=(ALL:ALL) NOPASSWD:${bumblebeeHack}
-    '';
+      nix = {
+        maxJobs = if cfg.hyperThreading then 8 else 4;
+        buildCores = if cfg.hyperThreading then 8 else 4;
+      };
 
-    services.xserver = {
-      displayManager.sessionCommands = lib.optionalString cfg.enableBumblebeeHack ''
-        sudo ${bumblebeeHack}
+      hardware = {
+        enableAllFirmware = true;
+        opengl.driSupport32Bit = true;
+        bluetooth.enable = true;
+        pulseaudio = {
+          package = pkgs.pulseaudioFull;
+          support32Bit = true;
+        };
+      };
+
+      services.xserver.videoDrivers = [ "intel" ];
+    }
+    (let
+      script = pkgs.writeScript "bumblebeeHack.sh" ''
+        #! ${pkgs.bash}/bin/bash
+        case "$1" in
+            start) systemctl start bumblebeed service ;;
+            stop)  systemctl stop  bumblebeed service ;;
+        esac
+      '';
+    in lib.mkIf cfg.enableBumblebeeHack {
+      hardware.bumblebee.enable = true;
+
+      systemd.services.bumblebeed.wantedBy = lib.mkForce [];
+
+      services.xserver.displayManager.sessionCommands = ''
+        sudo ${script} start
       '';
 
-      videoDrivers = [ "intel" ];
-    };
-  };
+      security.sudo.extraConfig = ''
+        %${config.users.groups.wheel.name} ALL=(ALL:ALL) NOPASSWD:${script}
+      '';
+
+      powerManagement = let
+        paused = "/tmp/.bumblebeed-paused";
+      in {
+        powerDownCommands = ''
+          if systemctl is-active bumblebeed; then
+              touch ${paused}
+              ${script} stop
+          fi
+        '';
+
+        resumeCommands = ''
+          [ -f ${paused} ] && rm ${paused} && ${script} start
+        '';
+
+        powerUpCommands = lib.optionalString (!config.boot.tmpOnTmpfs) ''
+          [ -f ${paused} ] && rm ${paused}
+        '';
+      };
+    })
+  ]);
 }
