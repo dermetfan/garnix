87a1705f36ff89a3f98a2236d1e2a7a5760602c5
Robin Stumm <serverkorken@gmail.com>
2017-09-30 02:55:50 +0200

    home-manager: update unstable

--- .config/nixpkgs/overlays/home-manager.nix
+++ .config/nixpkgs/overlays/home-manager.nix
@@ -4,8 +4,8 @@ self: super:
   home-manager-src = super.fetchFromGitHub {
     owner = "rycee";
     repo = "home-manager";
-    rev = "e1bceb2adb046d56b437d6c2c48fc717b0b028e3";
-    sha256 = "19smd3y6z2yvp6agmdk7li3pji4vpnvj899f898fgp4mp3b3q2j0";
+    rev = "e9deaf2ca5b414de4c5ed156fb801f05011f617a";
+    sha256 = "0zaqdzzwj2fzb7y8i0g0b5v15wwl55i497g579ksf4j01nl7clgi";
   };
 
   home-manager = import self.home-manager-src {
