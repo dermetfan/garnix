8bac051c7bb180752bfb61c9bc6675eedc39c23a
Robin Stumm <serverkorken@gmail.com>
2017-12-22 03:27:05 +0100

    home-manager: update

--- .config/nixpkgs/overlays/home-manager.nix
+++ .config/nixpkgs/overlays/home-manager.nix
@@ -4,8 +4,8 @@ self: super:
   home-manager-src = super.fetchFromGitHub {
     owner = "rycee";
     repo = "home-manager";
-    rev = "c023b0532a7c9761840aefc0e059b2e424fb1520";
-    sha256 = "1klsydx3cv0jcra2viczqa8y94xx4s1wfp6ibpbga6s1796y9nql";
+    rev = "e75b68e39195bda85ccb88541b89b4d75f86631a";
+    sha256 = "18rqnj0mcv42z7lmhia6vqfrj1ryywwpx077p5grxzld1mipin91";
   };
 
   home-manager =
