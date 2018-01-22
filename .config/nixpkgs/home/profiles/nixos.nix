39bf6cf0d53f2b5b1fe0b812ef12d50974415781
Robin Stumm <serverkorken@gmail.com>
2018-01-22 22:45:59 +0100

    fix nixos profile

--- .config/nixpkgs/home/profiles/nixos.nix
+++ .config/nixpkgs/home/profiles/nixos.nix
@@ -3,9 +3,11 @@
 let
   cfg = config.config.profiles.nixos;
 in {
+  imports = [ <nixpkgs/nixos/modules/misc/passthru.nix> ];
+
   options.config.profiles.nixos.enable = lib.mkEnableOption "nixos integration";
 
-  config = lib.mkIf cfg.enable {
-    passthru.systemConfig = (import <nixpkgs/nixos> {}).config;
-  };
+  config.passthru = if cfg.enable then {
+    systemConfig = (import <nixpkgs/nixos> {}).config;
+  } else {};
 }
