11475c74be00858cad26f148e469868c1094015b
Robin Stumm <serverkorken@gmail.com>
2018-05-24 16:14:05 +0200

    tmux: install some plugins

--- .config/nixpkgs/home/programs/tmux.nix
+++ .config/nixpkgs/home/programs/tmux.nix
@@ -15,9 +15,24 @@ in {
 
   config.home.packages = lib.optional config.programs.tmux.enable pkgs.tmux;
   config.home.file = lib.mkIf cfg.enable {
-    ".tmux.conf".text = ''
-      set-option -ga terminal-overrides ",xterm-256color:Tc"
+    ".tmux.conf".text = let
+      plugin = name: "run-shell '${pkgs.tmuxPlugins.${name}}/share/tmux-plugins/${name}/${name}.tmux'";
+    in ''
+      set-option -ga terminal-overrides ',xterm-256color:Tc'
       set-option -g default-terminal tmux-256color
+      set-option -g status-keys emacs
+      set-option -g focus-events on
+      set-option -g display-time 4000
+      set-option -s escape-time 0
+
+      set -g @resurrect-processes 'micro ~ranger ~"nixos-container root-login" ~"nixos-container run"'
+      set -g @resurrect-save-shell-history on
+      set -g @resurrect-capture-pane-contents on
+
+      ${plugin "resurrect"}
+      ${plugin "copycat"  }
+      ${plugin "sidebar"  }
+      ${plugin "yank"     }
     '';
   };
 }
