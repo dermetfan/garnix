ad1962334c0f3c44d8d8c8d0bcae2d7d24e78aff
Robin Stumm <serverkorken@gmail.com>
2019-02-01 19:41:48 +0100

    move machines to profiles

--- nixos/default.nix
+++ nixos/default.nix
@@ -9,10 +9,9 @@
     ./dns-watch.nix
     ./steam-controller.nix
 
-    machines/MSI_PE60-6QE.nix
-
     profiles/netbook.nix
     profiles/notebook.nix
+    profiles/machines/MSI_PE60-6QE.nix
   ];
 
   config = {
