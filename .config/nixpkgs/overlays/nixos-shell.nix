38e28d05c251ca98ce672409c0ff7ef68906b847
Robin Stumm <serverkorken@gmail.com>
2019-10-17 01:28:49 +0200

    nixos-shell: update

--- .config/nixpkgs/overlays/nixos-shell.nix
+++ .config/nixpkgs/overlays/nixos-shell.nix
@@ -2,7 +2,7 @@ self: super: {
   nixos-shell = super.fetchFromGitHub {
     owner = "Mic92";
     repo = "nixos-shell";
-    rev = "2d6b3b5074417b036eba8b44e6511fbfb49c07bd";
-    sha256 = "0y6bbfv7rmlm7sql8gfk0fi2q1fkf7ijwi4hshgc11pv2wb88dqb";
+    rev = "39936f8b60c6ff3644ae508af38505d39f97730c";
+    sha256 = "11aj2ck9kh4iaiidrsf8kxp6rqpghi0zw88hncmv5r37wcby7ika";
   };
 }
