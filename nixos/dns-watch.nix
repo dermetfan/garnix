08cfbb55e63a66652d1671bd2e2fff71de90e8cb
Robin Stumm <serverkorken@gmail.com>
2019-10-22 10:10:31 +0200

    dns.watch: add IPv6 servers

--- nixos/dns-watch.nix
+++ nixos/dns-watch.nix
@@ -7,8 +7,11 @@ in {
 
   config = lib.mkIf cfg.enable {
     networking.nameservers = [
-      "84.200.69.80"
-      "84.200.70.40"
+      "84.200.69.80" # resolver1.dns.watch
+      "84.200.70.40" # resolver2.dns.watch
+    ] ++ lib.optionals config.networking.enableIPv6 [
+      "2001:1608:10:25::1c04:b12f" # resolver1.dns.watch / resolver1v6.dns.watch
+      "2001:1608:10:25::9249:d69b" # resolver2.dns.watch / resolver2v6.dns.watch
     ];
   };
 }
