f9b883e2f16c8fafa9759ba63834a9f12f70c05a
Robin Stumm <serverkorken@gmail.com>
2020-12-02 01:11:15 +0100

    uhk: just apply home keyboard for simplicity

--- .config/nixpkgs/home/misc/uhk.nix
+++ .config/nixpkgs/home/misc/uhk.nix
@@ -2,12 +2,8 @@
 
 {
   wayland.windowManager.sway.config.input."7504:24866:Ultimate_Gadget_Laboratories_Ultimate_Hacking_Keyboard" = {
-    xkb_layout = with lib; concatStringsSep "," (
-      unique ( # reset layout in case it was changed
-        [ "us" ] ++
-        splitString "," config.home.keyboard.layout
-      )
-    );
+    xkb_layout = config.home.keyboard.layout;
+    xkb_variant = config.home.keyboard.variant;
     xkb_options = "compose:rctrl";
   };
 }
