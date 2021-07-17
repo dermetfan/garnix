12c142773ba7ebf68e2396966b25703f74005cd0
Robin Stumm <serverkorken@gmail.com>
2021-07-17 16:18:01 +0200

    roundcube: convert attrset to PHP config

--- nixops/config/default.nix
+++ nixops/config/default.nix
@@ -1,3 +1,5 @@
+{ lib, ... }:
+
 {
   imports = [
     <nixpkgs/nixos/modules/misc/passthru.nix>
@@ -20,5 +22,36 @@
       sslCertificate = ../../keys/host.crt;
       sslCertificateKey = "/run/keys/host.key";
     };
+
+    # XXX rewrite with lib.generators.toKeyValue?
+    # XXX is escapeShellArg really ok for PHP strings?
+    lib.generators.toPHP = {
+      variable ? null,
+    }: attrs:
+      with lib;
+      let
+        writeValue = value:
+          if isString value then escapeShellArg value else
+          if isInt value || isFloat value then toString value else
+          if isBool value then if value then "true" else "false" else
+          if isList value then "[\n" + (concatStringsSep ",\n" (map writeValue value)) + "\n]" else
+          if isAttrs value then "[\n" + (concatStringsSep ",\n" (mapAttrsToList (k: v: "${escapeShellArg k} => ${writeValue v}") value)) + "\n]" else
+          throw "value of unsupported type ${builtins.typeOf value}"
+        ;
+      in
+        if variable == null
+        then writeValue attrs
+        else concatStringsSep "\n" (
+          mapAttrsToList
+            (k: v: "${k} = ${v};")
+            (mapAttrs'
+              (k: v: nameValuePair
+                "\$${variable}[${escapeShellArg k}]"
+                (writeValue v)
+              )
+              attrs
+            )
+        )
+      ;
   };
 }
--- nixops/config/roundcube.nix
+++ nixops/config/roundcube.nix
@@ -8,6 +8,39 @@ in {
       type = types.path;
       default = "/var/lib/roundcube/enigma";
     };
+
+    config = mkOption {
+      type = types.attrs;
+      default = {
+        default_host = {
+          "ssl://imap.gmail.com:993" = "Gmail";
+          "ssl://imap.inf.h-brs.de:993" = "H-BRS FB Informatik";
+        };
+
+        smtp_server = {
+          "imap.gmail.com" = "ssl://smtp.%z:465";
+          "imap.inf.h-brs.de" = "tls://smtp.%z:587";
+        };
+        smtp_user = "%u";
+        smtp_pass = "%p";
+
+        draft_autosave = 60;
+        reply_mode = -1;
+        sig_separator = false;
+
+        newmail_notifier_sound = true;
+        newmail_notifier_desktop = true;
+        newmail_notifier_desktop_timeout = 5;
+
+        enigma_pgp_homedir = cfg.enigma.pgpHomedir;
+        enigma_pgp_binary = pkgs.gnupg + "/bin/gpg";
+        enigma_pgp_agent = pkgs.gnupg + "/bin/gpg-agent";
+        enigma_pgp_gpgconf = pkgs.gnupg + "/bin/gpgconf";
+        enigma_passwordless = true;
+
+        keyservers = ["keys.openpgp.org" "pgp.mit.edu" "pgp.key-server.io"];
+      };
+    };
   };
 
   config.services = lib.mkIf cfg.enable {
@@ -28,31 +61,7 @@ in {
 
       hostName = "roundcube." + config.passthru.domain;
 
-      extraConfig = ''
-        $config['default_host'] = [
-          'ssl://imap.gmail.com:993' => 'Gmail',
-        ];
-
-        $config['smtp_server'] = 'ssl://smtp.%z:465';
-        $config['smtp_user'] = '%u';
-        $config['smtp_pass'] = '%p';
-
-        $config['draft_autosave'] = 60;
-        $config['reply_mode'] = -1;
-        $config['sig_separator'] = false;
-
-        $config['newmail_notifier_sound'] = true;
-        $config['newmail_notifier_desktop'] = true;
-        $config['newmail_notifier_desktop_timeout'] = 5;
-
-        $config['enigma_pgp_homedir'] = ${lib.escapeShellArg cfg.enigma.pgpHomedir};
-        $config['enigma_pgp_binary'] = ${lib.escapeShellArg "${pkgs.gnupg}/bin/gpg"};
-        $config['enigma_pgp_agent'] = ${lib.escapeShellArg "${pkgs.gnupg}/bin/gpg-agent"};
-        $config['enigma_pgp_gpgconf'] = ${lib.escapeShellArg "${pkgs.gnupg}/bin/gpgconf"};
-        $config['enigma_passwordless'] = true;
-
-        $config['keyservers'] = ['keys.openpgp.org', 'pgp.mit.edu', 'pgp.key-server.io'];
-      '';
+      extraConfig = config.passthru.lib.generators.toPHP { variable = "config"; } cfg.config;
     };
 
     nginx.virtualHosts.${config.services.roundcube.hostName} = config.passthru."nginx.virtualHosts.withSSL" {};
