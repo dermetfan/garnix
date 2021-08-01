1906f2023f4820ad88749ad6803cbde4e25caca6
Robin Stumm <serverkorken@gmail.com>
2021-08-01 17:09:32 +0200

    dotfiles: zsh: use home-manager's autosuggestions

--- dotfiles/.config/nixpkgs/home/programs/zsh.nix
+++ dotfiles/.config/nixpkgs/home/programs/zsh.nix
@@ -20,6 +20,7 @@ in {
       zoxide.enable = true;
 
       zsh = {
+        enableAutosuggestions = true;
         enableCompletion = let
           sysZshCfg = config.passthru.systemConfig.programs.zsh or (
             builtins.trace
@@ -136,15 +137,6 @@ in {
               repo = "fast-syntax-highlighting";
               rev = "5fab542516579bdea5cc8b94137d9d85a0c3fda5";
               sha256 = "1ff1z2snbl9rx3mrcjbamlvc21fh9l32zi2hh9vcgcwbjwn5kikg";
-            };
-          }
-          {
-            name = "zsh-autosuggestions";
-            src = pkgs.fetchFromGitHub {
-              owner = "zsh-users";
-              repo = "zsh-autosuggestions";
-              rev = "v0.4.0";
-              sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
             };
           }
           {
