4675bc96e8da657c9f81bd62282040ca77603435
Robin Stumm <serverkorken@gmail.com>
2021-08-14 12:06:09 +0200

    dotfiles: nixos-shell: remove overlay (is now in nixpkgs)

--- dotfiles/.config/nixpkgs/overlays/nixos-shell.nix
+++
@@ -1,8 +0,0 @@
-self: super: {
-  nixos-shell = super.fetchFromGitHub {
-    owner = "Mic92";
-    repo = "nixos-shell";
-    rev = "60e8f7f41e5ac1ec7acf1ec2fc2fbdad38d78111";
-    sha256 = "0mc4cn4bkh2frkiswp29kwy033jyggkfgflmgrw5dfp2d0l06j4l";
-  };
-}
