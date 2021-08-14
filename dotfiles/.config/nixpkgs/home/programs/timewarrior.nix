ca0315d7b426f28b0c1d3b816e470bab39376bde
Robin Stumm <serverkorken@gmail.com>
2021-08-14 18:55:11 +0200

    dotfiles: timewarrior: add flexitime plugin

--- dotfiles/.config/nixpkgs/home/programs/timewarrior.nix
+++ dotfiles/.config/nixpkgs/home/programs/timewarrior.nix
@@ -14,11 +14,30 @@ in {
   config.home = {
     packages = lib.optional config.programs.timewarrior.enable pkgs.timewarrior;
 
-    file = lib.mkIf cfg.enable {
-      ".timewarrior/extensions/totals" = {
-        source = "${pkgs.timewarrior}/share/doc/timew/ext/totals.py";
-        executable = true;
-      };
-    };
+    file = lib.mkIf cfg.enable (
+      let
+        flexitime = pkgs.fetchFromGitHub {
+          owner = "AMNeumann";
+          repo = "flexitime";
+          rev = "84a1fe568af791af9f11f4df7e4f3d5fa4d0b8b1";
+          sha256 = "0nsxz472pd1iw0qxwyis0jhgrsjc7vrx9qwfczdz5vr6hzi9h5j9";
+        };
+      in {
+        ".timewarrior/extensions/totals" = {
+          source = "${pkgs.timewarrior}/share/doc/timew/ext/totals.py";
+          executable = true;
+        };
+
+        ".timewarrior/extensions/flexitime" = {
+          source = "${flexitime}/flexitime.py";
+          executable = true;
+        };
+
+        ".timewarrior/extensions/splittime" = {
+          source = "${flexitime}/splittime.py";
+          executable = true;
+        };
+      }
+    );
   };
 }
