ff722c7c1d9d8860960e9526080d6eb3f080d0a6
Robin Stumm <serverkorken@gmail.com>
2021-08-22 21:53:47 +0200

    nixpkgs: update dermetfan-homepage overlay

--- nixpkgs/overlays/dermetfan-homepage/dermetfan-homepage.nix
+++ nixpkgs/overlays/dermetfan-homepage/dermetfan-homepage.nix
@@ -4,6 +4,6 @@ import (fetchFromSourcehut {
   owner = "~dermetfan";
   repo = "dermetfan-blog";
   vc = "hg";
-  rev = "85f85cd2af90089405abe29b2c9e4008a3e09c59";
-  hash = "sha256-t5tzeRVMyxkwRM4fccYRFR5rQVxO6kXptj2cxOrnnQc=";
+  rev = "3be52f3fb523f07592790dded687d7104c5b9359";
+  hash = "sha256-zCP9zhkIKnBw4E558UR1vWHGnugejxV0LgLf+ccaraw=";
 }) {}
