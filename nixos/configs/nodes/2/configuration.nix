15fd86e8e0d06f293ee97c934d44b954baef08d4
Robin Stumm <serverkorken@gmail.com>
2021-10-24 17:30:18 +0200

    nixos: node-2: warn if initrd ssh host key is empty

--- nixos/configs/nodes/2/configuration.nix
+++ nixos/configs/nodes/2/configuration.nix
@@ -58,6 +58,13 @@
   # XXX Hack: We just want to decrypt it so we can use it below.
   bootstrap.secrets.initrd_ssh_host_ed25519_key.path = "/dev/null";
 
+  assertions = [ {
+    assertion = lib.any (key:
+      lib.fileContents key != ""
+    ) config.boot.initrd.network.ssh.hostKeys;
+    message = "At least one SSH host key for the initial ramdisk is empty!";
+  } ];
+
   boot = {
     kernelParams = [
       # https://nixos.wiki/wiki/NixOS_on_ZFS#Known_issues
