8d0b096869ea9494cfdd861a21f4e016c7d2ee5e
Robin Stumm <serverkorken@gmail.com>
2021-11-18 01:04:25 +0100

    nixos/configs/nodes/node-0: add yggdrasil peers

--- nixos/configs/nodes/node-0/configuration.nix
+++ nixos/configs/nodes/node-0/configuration.nix
@@ -18,6 +18,14 @@
     syncthing.enable = true;
     roundcube.config.enigma_pgp_homedir = "/data/roundcube/enigma";
     homepage.enable = true;
+
+    yggdrasil.config.Peers = [
+      "tcp://94.130.203.208:5999"
+      "tcp://bunkertreff.ddns.net:5454"
+      "tcp://phrl42.ydns.eu:8842"
+      "tcp://ygg.mkg20001.io:80"
+      "tcp://yugudorashiru.de:80"
+    ];
   };
 
   environment.systemPackages = with pkgs; [
