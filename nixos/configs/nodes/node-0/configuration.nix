b20b104bf49dc848a1cf740b6318a02380979704
Robin Stumm <serverkorken@gmail.com>
2021-11-18 01:04:48 +0100

    nixos/configs/nodes/node-0: disable roundcube, minecraft-server, hydra, and homepage

--- nixos/configs/nodes/node-0/configuration.nix
+++ nixos/configs/nodes/node-0/configuration.nix
@@ -7,17 +7,11 @@
 
   profiles = {
     afraid-freedns.enable = true;
-    roundcube.enable = true;
-    minecraft-server.enable = true;
-    ssmtp.enable = true;
-    hydra.enable = true;
     nextcloud.enable = true;
   };
 
   services = {
     syncthing.enable = true;
-    roundcube.config.enigma_pgp_homedir = "/data/roundcube/enigma";
-    homepage.enable = true;
 
     yggdrasil.config.Peers = [
       "tcp://94.130.203.208:5999"
