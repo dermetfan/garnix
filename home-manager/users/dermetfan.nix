b6541780b5672001cf06a9e524dc86971868c3e2
Robin Stumm <serverkorken@gmail.com>
2021-11-29 14:55:31 +0100

    home-manager/users/dermetfan: enable mimeo and XDG default apps

--- home-manager/users/dermetfan.nix
+++ home-manager/users/dermetfan.nix
@@ -47,6 +47,20 @@ in {
           "firefox"
         ];
       };
+
+      mimeo = {
+        enable = true;
+        xdgOpen = true;
+      };
+    };
+
+    xdg.mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "text/plain" = [ "kakoune.desktop" ];
+        "x-scheme-handler/http" = [ "firefox.desktop" ];
+        "x-scheme-handler/https" = [ "firefox.desktop" ];
+      };
     };
   };
 }
