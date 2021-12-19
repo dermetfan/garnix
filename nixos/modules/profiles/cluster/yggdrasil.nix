b954a18ba820b05adcad78d58dc23f48b6fd8c42
Robin Stumm <serverkorken@gmail.com>
2021-12-19 16:23:19 +0100

    nixos/modules/profiles/cluster/yggdrasil: only peer with other cluster nodes

--- nixos/modules/profiles/cluster/yggdrasil.nix
+++ nixos/modules/profiles/cluster/yggdrasil.nix
@@ -1,11 +1,21 @@
-{ config, lib, ... }:
+{ config, ... }:
+
+let
+  peersWithYggdrasil = builtins.filter
+    (config: config.profiles.yggdrasil.enable)
+    config.profiles.cluster.node.peers;
+in
 
 {
-  services.yggdrasil.mergeableConfig.Peers = lib.pipe config.profiles.cluster.node.peers [
-    (builtins.filter (config: config.profiles.yggdrasil.enable))
-    (map (
+  services.yggdrasil.mergeableConfig = {
+    Peers = map (
       { networking, profiles, ... }:
       "tcp://${networking.hostName}.hosts.${networking.domain}:${toString profiles.yggdrasil.port}"
-    ))
-  ];
+    ) peersWithYggdrasil;
+
+    AllowedPublicKeys = map (
+      { services, ... }:
+      services.yggdrasil.config.PublicKey
+    ) peersWithYggdrasil;
+  };
 }
