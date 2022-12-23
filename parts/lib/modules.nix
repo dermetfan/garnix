0fbdd475efd155f7b3b921fa7513e0ebb366787d
Robin Stumm <serverkorken@gmail.com>
2022-12-23 14:52:52 +0100

    lib/modules: add filterOptionValues

--- parts/lib/modules.nix
+++ parts/lib/modules.nix
@@ -1,6 +1,40 @@
 lib:
 
 rec {
+  /*
+  Like `filterAttrs` for values of a module evaluation.
+
+  The predicate function receives the path to the value,
+  its option declaration and the resolved value itself.
+  */
+  filterOptionValues = let
+    recurse = p: pred: options: values:
+      if !__isAttrs values || lib.isDerivation values
+      then values
+      else
+        __mapAttrs
+        (
+          k:
+            recurse
+            (p ++ [k])
+            pred
+            (
+              let
+                o = options.${k} or null;
+              in
+                if lib.isOption o
+                then o.type.getSubOptions []
+                else o
+            )
+        )
+        (
+          lib.filterAttrs
+          (k: pred (p ++ [k]) options.${k} or null)
+          values
+        );
+  in
+    recurse [];
+
   retainModuleArgs = old: new:
     if lib.isFunction old && lib.isFunction new
     then lib.setFunctionArgs new (lib.functionArgs old)
