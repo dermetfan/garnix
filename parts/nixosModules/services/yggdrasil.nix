02fd554637a31f03496f1b14b6625109b25cb2bc
Robin Stumm <serverkorken@gmail.com>
2023-01-04 00:56:43 +0100

    nixosModules/services/yggdrasil: update public peers

--- parts/nixosModules/services/yggdrasil.nix
+++ parts/nixosModules/services/yggdrasil.nix
@@ -9,9 +9,16 @@ in {
 
   config.services.yggdrasil.settings.Peers = lib.mkIf cfg.publicPeers.germany.enable [
     "tcp://94.130.203.208:5999"
-    "tcp://bunkertreff.ddns.net:5454"
     "tcp://phrl42.ydns.eu:8842"
     "tcp://ygg.mkg20001.io:80"
-    "tcp://yugudorashiru.de:80"
+    "tcp://ygg.mkg20001.io:80"
+    "tcp://gutsche.tech:8888"
+    "tcp://ygg1.mk16.de:1337?key=0000000087ee9949eeab56bd430ee8f324cad55abf3993ed9b9be63ce693e18a"
+    "tcp://ygg2.mk16.de:1337?key=000000d80a2d7b3126ea65c8c08fc751088c491a5cdd47eff11c86fa1e4644ae"
+    "tls://vpn.ltha.de:443?key=0000006149970f245e6cec43664bce203f2514b60a153e194f31e2b229a1339d"
+    "tls://de-fsn-1.peer.v4.yggdrasil.chaz6.com:4444"
+    "tcp://yggdrasil.su:62486"
+    "tls://x-fra-promo.sergeysedoy97.ru:65535"
+    "tls://87.251.77.39:65535"
   ];
 }
