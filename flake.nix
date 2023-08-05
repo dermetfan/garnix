8aaf453983638feb5218fb31ff897f40d573a490
Robin Stumm <serverkorken@gmail.com>
2023-08-05 18:13:05 +0200

    do not override flake input that does not exist

--- flake.nix
+++ flake.nix
@@ -19,10 +19,7 @@
     };
     home-manager = {
       url = "github:nix-community/home-manager/release-23.05";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        utils.follows = "flake-utils";
-      };
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager-shell = {
       url = "sourcehut:~dermetfan/home-manager-shell/release-23.05";
