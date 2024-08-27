69fe1ec775f63273e0fa950b51c6a7eaf44e071b
Robin Stumm <serverkorken@gmail.com>
2024-08-27 17:14:15 +0200

    homeManagerProfiles/dermetfan/programs/htop: configure

--- parts/homeManagerProfiles/dermetfan/programs/htop.nix
+++ parts/homeManagerProfiles/dermetfan/programs/htop.nix
@@ -3,16 +3,41 @@
 {
   programs.htop.settings = {
     hide_userland_threads = true;
+    hide_kernel_threads = true;
+    show_thread_names = true;
     show_program_path = false;
+    show_merged_command = true;
+    header_margin = false;
+    screen_tabs = true;
     tree_view = true;
+    cpu_count_from_one = true;
+    show_cpu_frequency = true;
+    show_cpu_temperature = true;
+    fields = with config.lib.htop.fields; [
+      PID
+      USER
+      PRIORITY
+      NICE
+      M_SWAP
+      M_SIZE
+      M_RESIDENT
+      M_SHARE
+      STATE
+      PERCENT_CPU
+      PERCENT_MEM
+      TIME
+      COMM
+    ];
   } // (with config.lib.htop;
     (leftMeters [
-      (bar "LeftCPUs")
-      (bar "Memory")
-      (bar "Swap")
+      (bar "AllCPUs2")
+      (bar "MemorySwap")
+      (bar "Zram")
     ]) //
     (rightMeters [
-      (bar "RightCPUs")
+      (bar "ZFSARC")
+      (text "DiskIO")
+      (text "NetworkIO")
       (text "Tasks")
       (text "LoadAverage")
       (text "Uptime")
