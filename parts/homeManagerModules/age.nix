cb8922c32f97bdca9ca471439ccc1ef441f8beb2
Robin Stumm <serverkorken@gmail.com>
2025-01-16 11:38:04 +0100

    homeManagerModules/age: fix eval error when agenix module is not imported

--- parts/homeManagerModules/age.nix
+++ parts/homeManagerModules/age.nix
@@ -4,10 +4,15 @@ let
   cfg = config.age.sensible;
 in {
   options.age.sensible = lib.mkEnableOption "sensible defaults" // {
-    default = options ? age;
+    default = true;
   };
 
-  config = lib.mkIf cfg {
-    age.secretsDir = lib.mkDefault "${config.xdg.configHome}/agenix";
-  };
+  config = lib.mkIf cfg (
+    # `mkIf` still checks that all options exist before resolving its condition
+    # so we need to eagerly check for the existance of the age option.
+    # This is needed to avoid errors when the agenix module is not imported.
+    lib.optionalAttrs (options ? age.secretsDir) {
+      age.secretsDir = lib.mkDefault "${config.xdg.configHome}/agenix";
+    }
+  );
 }
