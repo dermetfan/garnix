da775d632980ef15c04de910556e7c035ae048e6
Robin Stumm <serverkorken@gmail.com>
2025-08-19 23:22:19 +0200

    homeManagerProfiles/dermetfan/environments/admin: fix programs not enabled

--- parts/homeManagerProfiles/dermetfan/environments/admin.nix
+++ parts/homeManagerProfiles/dermetfan/environments/admin.nix
@@ -3,50 +3,52 @@
 {
   options.profiles.dermetfan.environments.admin.enable.default = false;
 
-  config.home.packages = with pkgs; [
-    bashmount
-    bandwhich
-    bottom
-    binutils
-    curl
-    entr
-    fd
-    file
-    jnv
-    lsof
-    ncdu
-    nix-diff
-    nix-du
-    nix-output-monitor
-    nix-prefetch-scripts
-    procs
-    pv
-    rsync
-    sd
-    socat
-    watchexec
-  ] ++ lib.optionals config.profiles.dermetfan.environments.gui.enable [
-    tigervnc
-    nix-query-tree-viewer
-  ] ++ lib.optionals stdenv.isLinux [
-    diffoscope
-    ftop
-    sysfsutils
-    mdcat
-    pciutils
-    psmisc
-    progress
-    sshfs-fuse
-    tty-share
-  ] ++ lib.optionals (config.profiles.dermetfan.environments.gui.enable && stdenv.isLinux) [
-    gpa
-  ];
+  config = {
+    home.packages = with pkgs; [
+      bashmount
+      bandwhich
+      bottom
+      binutils
+      curl
+      entr
+      fd
+      file
+      jnv
+      lsof
+      ncdu
+      nix-diff
+      nix-du
+      nix-output-monitor
+      nix-prefetch-scripts
+      procs
+      pv
+      rsync
+      sd
+      socat
+      watchexec
+    ] ++ lib.optionals config.profiles.dermetfan.environments.gui.enable [
+      tigervnc
+      nix-query-tree-viewer
+    ] ++ lib.optionals stdenv.isLinux [
+      diffoscope
+      ftop
+      sysfsutils
+      mdcat
+      pciutils
+      psmisc
+      progress
+      sshfs-fuse
+      tty-share
+    ] ++ lib.optionals (config.profiles.dermetfan.environments.gui.enable && stdenv.isLinux) [
+      gpa
+    ];
 
-  programs = {
-    bat.enable = true;
-    jq.enable = true;
-    nix-index.enable = true;
-    ripgrep.enable = true;
-    skim.enable = true;
+    programs = {
+      bat.enable = true;
+      jq.enable = true;
+      nix-index.enable = true;
+      ripgrep.enable = true;
+      skim.enable = true;
+    };
   };
 }
