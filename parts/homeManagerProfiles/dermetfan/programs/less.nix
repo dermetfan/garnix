87eb4b1dfdc6d4adf119b33e3ed3e8014dce00fc
Robin Stumm <serverkorken@gmail.com>
2025-08-29 22:58:04 +0200

    homeManagerProfiles/dermetfan/programs/less: configure keybindings, enable lesspipe

--- parts/homeManagerProfiles/dermetfan/programs/less.nix
+++ parts/homeManagerProfiles/dermetfan/programs/less.nix
@@ -1,3 +1,34 @@
 {
-  home.sessionVariables.LESS = "-RiW -#.05 -n4 -z-3";
+  home.sessionVariables.LESS = toString [
+    "--RAW-CONTROL-CHARS"
+    "--ignore-case"
+    "--hilite-unread"
+    "--shift .05"
+    "--line-numbers"
+    "--window -3"
+    "--tabs 4"
+  ];
+
+  programs = {
+    less = {
+      enable = true;
+      keys = ''
+        #command
+        i     forw-line
+        I     forw-line-force
+        r     back-line
+        R     back-line-force
+        n     left-scroll
+        o     right-scroll
+        j     repeat-search
+        \ej   repeat-search-all
+        J     reverse-search
+        \eJ   reverse-search-all
+        ^O^J  osc8-forw-search
+        ^Oj   osc8-forw-search
+      '';
+    };
+
+    lesspipe.enable = true;
+  };
 }
