dermetfan/garnix

commit 1cd1748bd57d78744b320f9432223903f89e37bf

homeManagerProfiles/dermetfan/i3-sway: minor refactoring

parts/homeManagerProfiles/dermetfan/i3-sway.nix
@@ -6,12 +6,10 @@ let
   };
 
   commonConfig = getCfg: let
-    up    = "r";
-    down  = "i";
-    left  = "n";
-    right = "o";
     cfg = getCfg config;
     profileCfg = getCfg config.profiles.dermetfan;
+
+    inherit (cfg.config) modifier left right up down terminal;
   in lib.mkMerge [
     {
       focus.followMouse = false;
@@ -49,106 +47,114 @@ let
         '';
       } ];
 
+      terminal = "alacritty";
+
       modifier = "Mod4";
 
+      left  = "n";
+      right = "o";
+      up    = "r";
+      down  = "i";
+
       keybindings = let
-        mod = cfg.config.modifier;
         floatingMoveStep = "25px";
       in {
-        "${mod}+Return" = "exec alacritty";
+        "${modifier}+Return" = "exec ${terminal}";
 
-        "${mod}+x" = "exec alacritty -e ranger";
-        "${mod}+Shift+x" = "exec alacritty -e sudo ranger";
+        "${modifier}+x" = "exec ${terminal} -e ranger";
+        "${modifier}+Shift+x" = "exec ${terminal} -e sudo ranger";
 
-        "${mod}+e"       = "exec rofi -show drun";
-        "${mod}+Shift+e" = "exec rofi -show combi";
-        "${mod}+Tab"     = "exec rofi -show window";
+        "${modifier}+e"       = "exec rofi -show drun";
+        "${modifier}+Shift+e" = "exec rofi -show combi";
+        "${modifier}+Tab"     = "exec rofi -show window";
 
-        "${mod}+m" = lib.mkIf config.programs.networkmanager-dmenu.enable "exec networkmanager_dmenu";
+        "${modifier}+m" = lib.mkIf config.programs.networkmanager-dmenu.enable "exec networkmanager_dmenu";
 
-        "${mod}+q" = "kill";
+        "${modifier}+q" = "kill";
 
-        "${mod}+${left}"  = "focus left";
-        "${mod}+${right}" = "focus right";
-        "${mod}+${up}"    = "focus up";
-        "${mod}+${down}"  = "focus down";
-        "${mod}+Left"  = "focus left";
-        "${mod}+Right" = "focus right";
-        "${mod}+Up"    = "focus up";
-        "${mod}+Down"  = "focus down";
+        "${modifier}+${left}"  = "focus left";
+        "${modifier}+${right}" = "focus right";
+        "${modifier}+${up}"    = "focus up";
+        "${modifier}+${down}"  = "focus down";
+        "${modifier}+Left"  = "focus left";
+        "${modifier}+Right" = "focus right";
+        "${modifier}+Up"    = "focus up";
+        "${modifier}+Down"  = "focus down";
 
-        "${mod}+Shift+${left}"  = "move left ${floatingMoveStep}";
-        "${mod}+Shift+${right}" = "move right ${floatingMoveStep}";
-        "${mod}+Shift+${up}"    = "move up ${floatingMoveStep}";
-        "${mod}+Shift+${down}"  = "move down ${floatingMoveStep}";
-        "${mod}+Shift+Left"  = "move left ${floatingMoveStep}";
-        "${mod}+Shift+Right" = "move right ${floatingMoveStep}";
-        "${mod}+Shift+Up"    = "move up ${floatingMoveStep}";
-        "${mod}+Shift+Down"  = "move down ${floatingMoveStep}";
+        "${modifier}+Shift+${left}"  = "move left ${floatingMoveStep}";
+        "${modifier}+Shift+${right}" = "move right ${floatingMoveStep}";
+        "${modifier}+Shift+${up}"    = "move up ${floatingMoveStep}";
+        "${modifier}+Shift+${down}"  = "move down ${floatingMoveStep}";
+        "${modifier}+Shift+Left"  = "move left ${floatingMoveStep}";
+        "${modifier}+Shift+Right" = "move right ${floatingMoveStep}";
+        "${modifier}+Shift+Up"    = "move up ${floatingMoveStep}";
+        "${modifier}+Shift+Down"  = "move down ${floatingMoveStep}";
 
-        "${mod}+f" = "fullscreen toggle";
+        "${modifier}+f" = "fullscreen toggle";
 
-        "${mod}+b"       =                         "border toggle";
-        "${mod}+Shift+b" = "[workspace=__focused__] border toggle";
+        "${modifier}+b"       =                         "border toggle";
+        "${modifier}+Shift+b" = "[workspace=__focused__] border toggle";
 
-        "${mod}+s" = "layout stacking";
-        "${mod}+w" = "layout tabbed";
-        "${mod}+d" = "layout toggle split";
+        "${modifier}+s" = "layout stacking";
+        "${modifier}+w" = "layout tabbed";
+        "${modifier}+d" = "layout toggle split";
 
-        "${mod}+h" = "split h";
-        "${mod}+v" = "split v";
+        "${modifier}+h" = "split h";
+        "${modifier}+v" = "split v";
 
-        "${mod}+Shift+space"            = "floating toggle";
-        "--whole-window ${mod}+button2" = "floating toggle";
+        "${modifier}+Shift+space"            = "floating toggle";
+        "--whole-window ${modifier}+button2" = "floating toggle";
 
-        "${mod}+space" = "focus mode_toggle";
+        "${modifier}+space" = "focus mode_toggle";
 
-        "${mod}+a" = "focus parent";
-        "${mod}+z" = "focus child";
+        "${modifier}+a" = "focus parent";
+        "${modifier}+z" = "focus child";
 
-        "${mod}+t" = "scratchpad show";
-        "${mod}+Shift+t" = "move scratchpad";
+        "${modifier}+t" = "scratchpad show";
+        "${modifier}+Shift+t" = "move scratchpad";
 
-        "${mod}+1" = "workspace number 1";
-        "${mod}+2" = "workspace number 2";
-        "${mod}+3" = "workspace number 3";
-        "${mod}+4" = "workspace number 4";
-        "${mod}+5" = "workspace number 5";
-        "${mod}+6" = "workspace number 6";
-        "${mod}+7" = "workspace number 7";
-        "${mod}+8" = "workspace number 8";
-        "${mod}+9" = "workspace number 9";
-        "${mod}+0" = "workspace number 10";
+        "${modifier}+1" = "workspace number 1";
+        "${modifier}+2" = "workspace number 2";
+        "${modifier}+3" = "workspace number 3";
+        "${modifier}+4" = "workspace number 4";
+        "${modifier}+5" = "workspace number 5";
+        "${modifier}+6" = "workspace number 6";
+        "${modifier}+7" = "workspace number 7";
+        "${modifier}+8" = "workspace number 8";
+        "${modifier}+9" = "workspace number 9";
+        "${modifier}+0" = "workspace number 10";
 
-        "${mod}+Shift+1" = "move container to workspace number 1";
-        "${mod}+Shift+2" = "move container to workspace number 2";
-        "${mod}+Shift+3" = "move container to workspace number 3";
-        "${mod}+Shift+4" = "move container to workspace number 4";
-        "${mod}+Shift+5" = "move container to workspace number 5";
-        "${mod}+Shift+6" = "move container to workspace number 6";
-        "${mod}+Shift+7" = "move container to workspace number 7";
-        "${mod}+Shift+8" = "move container to workspace number 8";
-        "${mod}+Shift+9" = "move container to workspace number 9";
-        "${mod}+Shift+0" = "move container to workspace number 10";
+        "${modifier}+Shift+1" = "move container to workspace number 1";
+        "${modifier}+Shift+2" = "move container to workspace number 2";
+        "${modifier}+Shift+3" = "move container to workspace number 3";
+        "${modifier}+Shift+4" = "move container to workspace number 4";
+        "${modifier}+Shift+5" = "move container to workspace number 5";
+        "${modifier}+Shift+6" = "move container to workspace number 6";
+        "${modifier}+Shift+7" = "move container to workspace number 7";
+        "${modifier}+Shift+8" = "move container to workspace number 8";
+        "${modifier}+Shift+9" = "move container to workspace number 9";
+        "${modifier}+Shift+0" = "move container to workspace number 10";
 
-        "${mod}+Ctrl+Left"  = "move workspace to output left";
-        "${mod}+Ctrl+Right" = "move workspace to output right";
-        "${mod}+Ctrl+Up"    = "move workspace to output up";
-        "${mod}+Ctrl+Down"  = "move workspace to output down";
+        "${modifier}+Ctrl+Left"  = "move workspace to output left";
+        "${modifier}+Ctrl+Right" = "move workspace to output right";
+        "${modifier}+Ctrl+Up"    = "move workspace to output up";
+        "${modifier}+Ctrl+Down"  = "move workspace to output down";
 
-        "${mod}+Ctrl+${up}"    = "resize grow height 10 px or 10 ppt";
-        "${mod}+Ctrl+${down}"  = "resize shrink height 10 px or 10 ppt";
-        "${mod}+Ctrl+${right}" = "resize grow width 10 px or 10 ppt";
-        "${mod}+Ctrl+${left}"  = "resize shrink width 10 px or 10 ppt";
-        "${mod}+Ctrl+Shift+${up}"    = "resize grow height 1 px or 1 ppt";
-        "${mod}+Ctrl+Shift+${down}"  = "resize shrink height 1 px or 1 ppt";
-        "${mod}+Ctrl+Shift+${right}" = "resize grow width 1 px or 1 ppt";
-        "${mod}+Ctrl+Shift+${left}"  = "resize shrink width 1 px or 1 ppt";
+        "${modifier}+Ctrl+${up}"    = "resize grow height 10 px or 10 ppt";
+        "${modifier}+Ctrl+${down}"  = "resize shrink height 10 px or 10 ppt";
+        "${modifier}+Ctrl+${right}" = "resize grow width 10 px or 10 ppt";
+        "${modifier}+Ctrl+${left}"  = "resize shrink width 10 px or 10 ppt";
+        "${modifier}+Ctrl+Shift+${up}"    = "resize grow height 1 px or 1 ppt";
+        "${modifier}+Ctrl+Shift+${down}"  = "resize shrink height 1 px or 1 ppt";
+        "${modifier}+Ctrl+Shift+${right}" = "resize grow width 1 px or 1 ppt";
+        "${modifier}+Ctrl+Shift+${left}"  = "resize shrink width 1 px or 1 ppt";
       };
     }
 
     (let
       modeGaps = "gaps: i|r (± inner), n|o (± outer), u|l (0 inner/outer), d (default), + Shift (global), b|B (bar)";
+
+      inherit (cfg.config.gaps) inner;
     in lib.mkIf profileCfg.enableGaps {
       gaps = {
         inner = 15;
@@ -156,7 +162,7 @@ let
         smartBorders = "on";
       };
 
-      keybindings."${cfg.config.modifier}+g" = ''mode "${modeGaps}"; bar hidden_state show'';
+      keybindings."${modifier}+g" = ''mode "${modeGaps}"; bar hidden_state show'';
 
       modes.${modeGaps} = {
         "n" = "gaps outer current minus 5";
@@ -165,8 +171,8 @@ let
         "r" = "gaps inner current plus 5";
         "l" = "gaps outer current set 0";
         "u" = "gaps inner current set 0";
-        "d" = "gaps outer current set 0; gaps inner current set ${toString cfg.config.gaps.inner}; bar bar-0 gaps 0";
-        "b" = "bar bar-0 gaps 0 ${toString cfg.config.gaps.inner} ${toString cfg.config.gaps.inner} ${toString cfg.config.gaps.inner}";
+        "d" = "gaps outer current set 0; gaps inner current set ${toString inner}; bar bar-0 gaps 0";
+        "b" = "bar bar-0 gaps 0 ${toString inner} ${toString inner} ${toString inner}";
 
         "Shift+${left}"  = "gaps outer all minus 5";
         "Shift+${up}"    = "gaps inner all plus 5";
@@ -174,7 +180,7 @@ let
         "Shift+${right}" = "gaps outer all plus 5";
         "Shift+u" = "gaps inner all set 0";
         "Shift+l" = "gaps outer all set 0";
-        "Shift+d" = "gaps outer all set 0; gaps inner all set ${toString cfg.config.gaps.inner}; bar bar-0 gaps 0";
+        "Shift+d" = "gaps outer all set 0; gaps inner all set ${toString inner}; bar bar-0 gaps 0";
         "Shift+b" = "bar bar-0 gaps 0";
 
         "Return" = ''mode "default"; bar hidden_state hide'';

commit e2edf46a63bf18e3edeaa508e41d7f7df605db94

homeManagerProfiles/dermetfan/i3-sway: fix eval errors
introduced in 1cd1748bd57d78744b320f9432223903f89e37bf

parts/homeManagerProfiles/dermetfan/i3-sway.nix
@@ -9,7 +9,12 @@ let
     cfg = getCfg config;
     profileCfg = getCfg config.profiles.dermetfan;
 
-    inherit (cfg.config) modifier left right up down terminal;
+    inherit (cfg.config) modifier terminal;
+
+    left  = "n";
+    right = "o";
+    up    = "r";
+    down  = "i";
   in lib.mkMerge [
     {
       focus.followMouse = false;
@@ -50,11 +55,6 @@ let
       terminal = "alacritty";
 
       modifier = "Mod4";
-
-      left  = "n";
-      right = "o";
-      up    = "r";
-      down  = "i";
 
       keybindings = let
         floatingMoveStep = "25px";
@@ -152,7 +152,7 @@ let
     }
 
     (let
-      modeGaps = "gaps: i|r (± inner), n|o (± outer), u|l (0 inner/outer), d (default), + Shift (global), b|B (bar)";
+      modeGaps = "gaps: ${down}|${up} (± inner), ${left}|${right} (± outer), u|l (0 inner/outer), d (default), + Shift (global), b|B (bar)";
 
       inherit (cfg.config.gaps) inner;
     in lib.mkIf profileCfg.enableGaps {
@@ -165,10 +165,10 @@ let
       keybindings."${modifier}+g" = ''mode "${modeGaps}"; bar hidden_state show'';
 
       modes.${modeGaps} = {
-        "n" = "gaps outer current minus 5";
-        "o" = "gaps outer current plus 5";
-        "i" = "gaps inner current minus 5";
-        "r" = "gaps inner current plus 5";
+        ${left} = "gaps outer current minus 5";
+        ${right} = "gaps outer current plus 5";
+        ${down} = "gaps inner current minus 5";
+        ${up} = "gaps inner current plus 5";
         "l" = "gaps outer current set 0";
         "u" = "gaps inner current set 0";
         "d" = "gaps outer current set 0; gaps inner current set ${toString inner}; bar bar-0 gaps 0";

parts/homeManagerProfiles/dermetfan/wayland/windowManager/sway.nix
@@ -55,6 +55,11 @@
           "hide_cursor " = "when-typing enable";
         };
 
+        left  = "n";
+        right = "o";
+        up    = "r";
+        down  = "i";
+
         keybindings = let
           mod = config.wayland.windowManager.sway.config.modifier;
           notifyVolume = pkgs.writers.writeNu "sway-notify-volume" ''